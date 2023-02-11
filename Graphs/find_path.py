9af73bb01ab400ddf6c5ff430f9b41ebf3e5d83f
Fuad Goloba <fuadgoloba@Fuads-MacBook-Pro.local>
2023-02-11 15:30:50 +0000

    Graphs

---
+++ Graphs/find_path.py
@@ -0,0 +1,57 @@
+# Count the unique paths from source to destination. 
+
+# Given a grid/matrix, count the unique paths from the top left to the bottom right.
+# Note that a single path only move along 0s and can't visit the same cell more than once. So 1 represents block and 0 represents a pass
+
+
+def hasPathsDFS(grid, row_pointer, col_pointer, visited):
+    '''
+        Using DFS (backtracking); Time complexity -> O(m * n) cause we brute-force by going through all possible coordinates that can lead to a path
+                   Space complexity -> 0(m * n)
+    '''
+    # Get the length of the rows and cols in the matrix 
+    row_len, col_len = len(grid), len(grid[0])
+    
+    
+    # Base Case
+        # Setting conditions to spot traversals that don't lead to a path
+            # Check if we're outside of the matrix (on the -ve axes)
+            # Check if we're outside of the matrix (on the +ve axes)
+            # Check if the coordinate has been visited
+            # Check if it's a blocked coordinate (i.e a 1)
+    if min(row_pointer, col_pointer) < 0 \
+        or row_pointer == row_len or col_pointer == col_len \
+        or (row_pointer, col_pointer) in visited \
+        or grid[row_pointer][col_pointer] == 1: 
+        return 0
+    
+        # Condition for when we find a path that has led to the destination
+    if row_pointer == row_len - 1 and col_pointer == col_len - 1:
+        return 1
+    
+    # Add the coordinates to visted coordinates
+    visited.add((row_pointer, col_pointer))
+    
+    # Initialise a counter to count unique paths
+    count = 0
+    
+    # Recursively traverse all possible cooordinates from the present location (i.e up, down, left, right)
+    count += hasPathsDFS(grid, row_pointer - 1, col_pointer, visited)
+    count += hasPathsDFS(grid, row_pointer + 1, col_pointer, visited)
+    count += hasPathsDFS(grid, row_pointer, col_pointer - 1, visited)
+    count += hasPathsDFS(grid, row_pointer, col_pointer + 1, visited)
+    
+    # Once we get to the destination, we backtrack (now starting from the destination to the source) to find if alternative paths could have led to the destination
+    # So we remove coordinated that have been visited in order to backtrack
+    visited.remove((row_pointer, col_pointer))
+    return count
+    
+if __name__ == '__main__':
+    matrix = [[0,0,0,0],
+              [1,1,0,0],
+              [0,0,0,1],
+              [0,1,0,0]]
+    visited = set()
+
+    print(hasPathsDFS(matrix, 0, 0, visited))
+    
