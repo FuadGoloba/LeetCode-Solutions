e1c554ebeca18b1132b3a16c0de17e6c610b00ed
FuadGoloba <fuadgoloba@gmail.com>
2024-12-02 09:19:36 +0000

    Two Pointers: container with most water

--- Two Pointers/Medium/container_with_most_water.py
+++ Two Pointers/Medium/container_with_most_water.py
@@ -1,16 +1,22 @@
-# Container with most water
+"""Container with most water
 
-# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-# Find two lines that together with the x-axis form a container, such that the container contains the most water. Return the maximum amount of water a container can store. # Notice that you may not slant the container.
+    You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+    Find two lines that together with the x-axis form a container, such that the container contains the most water. Return the maximum amount of water a container can store. # Notice that you may not slant the container.
 
-# Example 1
-# Input: height = [1,8,6,2,5,4,8,3,7]
-# Output: 49
-# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+    Example 1
+    Input: height = [1,8,6,2,5,4,8,3,7]
+    Output: 49
+    Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
-# Example 2:
-# Input: height = [1,1]
-# Output: 1
+    Example 2:
+    Input: height = [1,1]
+    Output: 1
+
+    SOLUTION:
+    The intuition is to use a two pointer technique. Two pointers (l, r) are initialised at both ends of the array. The technique starts with the widest container and moves the pointers inward based on the comparison of heights.
+    Increasing the width of the container can only lead to a larger area if the height of the new boundary is greater. By moving the pointers towards the center, we explore containers with the potential for greater areas.
+    The area is computed by the multiplication of the width and minimum height, ie (r - l) * min(height[l], height[r])
+"""
 
 def maxArea1(height):
     '''
@@ -33,12 +39,12 @@ def maxArea2(height):
     result = 0
     left, right = 0, len(height) - 1 # Initialise two pointers at the start and end of the array;
     
-    # Check that we haven't shifted our pointers to be equal to onbe another
+    # Starting from opposite ends of the array/container, check that we haven't shifted our pointers to be equal to one another
     while left < right:
         area = (right - left) * min(height[left], height[right]) # Compute the area
         result = max(area, result) # Update the area if there exists a better solution
         
-        # Shift the pointer that has the minimum height to its adjacent position (either left or right depending on which is minimum)
+        # Move the pointers inward to the center by shifting the pointer that has the minimum height to its adjacent position (either left or right depending on which is minimum)
         if height[left] < height[right]:
             left += 1
         else:
@@ -47,5 +53,5 @@ def maxArea2(height):
     return result
 
 if __name__ == '__main__':
-    for height in [[1,8,6,2,5,4,8,3,7], [1,1]]:
+    for height in [[1,8,6,2,5,4,8,3,7], [1,1], [1,2,1]]:
         print(maxArea2(height))
