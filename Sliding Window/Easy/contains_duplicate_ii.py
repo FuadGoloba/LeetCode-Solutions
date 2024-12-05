10bfce42b61e2185d42e5c3ea58bc01a7905c017
FuadGoloba <fuadgoloba@gmail.com>
2024-12-05 08:46:25 +0000

    Sliding Window: contains duplicate ii

---
+++ Sliding Window/Easy/contains_duplicate_ii.py
@@ -0,0 +1,45 @@
+"""
+    Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+    Example 1:
+    Input: nums = [1,2,3,1], k = 3
+    Output: true
+    
+    Example 2:
+    Input: nums = [1,0,1,1], k = 1
+    Output: true
+    
+    Example 3:
+    Input: nums = [1,2,3,1,2,3], k = 2
+    Output: false
+"""
+
+def containsNearbyDuplicate(nums: list[int], k: int) -> bool:
+    """
+        Using SLiding Window Technique and hashset, Time = O(n), Memory = O(n)
+        
+        SOLUTION:
+            The intuition is to use a sliding window technique and a hashset. We create a window, the size of k and find duplicates only within that window
+            The hashset helps in storing elements and performing O(1) loookups and detecting elements within a window that already exists.
+            Once we exceed traversal without finding duplicates in a window, we shift our window from the left by 1 and remove the left most element from the window hashset and try to find duplicates within the next window
+    """
+    window_set = set()
+    l = 0
+    r = 0
+
+    while r < len(nums):
+        # Check if we are still within the fixed window size; otherwise shift window and remove the left most element from window_set
+        if (r - l) > k:
+            window_set.remove(nums[l])
+            l += 1
+        
+        if nums[r] in window_set:
+            return True
+        # Add values within the current window to the hash set if it doesn;t exist
+        window_set.add(nums[r])
+        r += 1
+    return False
+
+if __name__ == '__main__':
+    for nums, k in [ ([1,2,3,1], 3), ([1,0,1,1], 1), ([1,2,3,1,2,3], 2) ]:
+        print(containsNearbyDuplicate(nums, k))
