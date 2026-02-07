9b79105a2a877286652074f0b2f6f5a5ece2e95f
FuadGoloba <fuadgoloba@gmail.com>
2026-02-07 23:57:35 +0000

    Two Pointers: remove element

--- Two Pointers/Easy/remove_element.py
+++ Two Pointers/Easy/remove_element.py
@@ -20,24 +20,27 @@
     Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
     Note that the five elements can be returned in any order.
     It does not matter what you leave beyond the returned k (hence they are underscores).
-    
-    SOLUTION:
-    The intuition is to use to two pointers; left and right. The right pointer is used to traverse the array while the left pointer keeps track of the the position where the next non-target element should be placed (is also k)
-    Both pointers are initialised to 0. During traversal by the right pointer, we overwrite the target elements with non-target elements at the position of the left pointer if not an occurrence of the target val, which then
-    effectively removes all occurrences of the target value from the array.
 """
 
 def removeElement(nums: list[int], val: int) -> int:
-    """Remove elements from Array using while loop
+    """Remove elements from Array using Two Pointers
+    
+    Intuition:
+        - Use two pointers to traverse the array. The right pointer iterates through the array, while the left pointer tracks the position where the next non-target element should be placed.
+        - When the right pointer encounters a non-target element, it is copied to the position indicated by the left pointer, and the left pointer is incremented. This effectively overwrites target elements with non-target elements as the right pointer traverses the array.
+        - After the right pointer has traversed the entire array, the left pointer will indicate the number of non-target elements in the array, which is returned as the final result. 
+    
+    Steps:
+        1. Initialize two pointers, left_ptr and right_ptr, to the start of the array.
+        2. While the right pointer is within the bounds of the array:
+            - If the element at the right pointer is not equal to the target value:
+                - Copy the element at the right pointer to the position indicated by the left pointer.
+                - Increment the left pointer to move to the next position for potential non-target elements.
+            - Increment the right pointer to continue traversing the array.
+        3. Return the value of the left pointer, which represents the count of non-target elements in the array.
     
     Time Complexity - O(n)
-
-    Args:
-        nums (list[int]): integer array
-        val (int): target integer value
-
-    Returns:
-        int: number of non-target values in nums
+    Space Complexity - O(1)
     """
     left_ptr, right_ptr = 0, 0 # left tracks position where the next non-target element should be placed and right traverses the array
     nums_length = len(nums)
@@ -52,21 +55,28 @@ def removeElement(nums: list[int], val: int) -> int:
 
 
 def removeElement2(nums: list[int], val: int) -> int:
-    """Remove elements from Array using for loop
+    """Remove elements from Array using Two Pointers (For Loop)
+    
+    Intuition:
+        - Use two pointers to traverse the array. The right pointer iterates through the array, while the left pointer tracks the position where the next non-target element should be placed.
+        - When the right pointer encounters a non-target element, it is copied to the position indicated by the left pointer, and the left pointer is incremented. This effectively overwrites target elements with non-target elements as the right pointer traverses the array.
+        - After the right pointer has traversed the entire array, the left pointer will indicate the number of non-target elements in the array, which is returned as the final result.
+        
+    Steps:
+        1. Initialize a pointer, left_ptr, to the start of the array to track the position where the next non-target element should be placed.
+        2. Use a for loop to iterate through the array with a right pointer:
+            - If the element at the right pointer is not equal to the target value:
+                - Copy the element at the right pointer to the position indicated by the left pointer.
+                - Increment the left pointer to move to the next position for potential non-target elements.
+        3. Return the value of the left pointer, which represents the count of non-target elements in the array.
     
     Time Complexity - O(n)
-
-    Args:
-        nums (list[int]): integer array
-        val (int): target integer value
-
-    Returns:
-        int: number of non-target values in nums
+    Space Complexity - O(1)
     """
     left_ptr = 0
     nums_length = len(nums)
-    
-    # Traverse the array and overwrite target elements with nonj-target elements
+
+    # Traverse the array and overwrite target elements with non-target elements
     for right_ptr in range(nums_length):
         if nums[right_ptr] != val:
             nums[left_ptr] = nums[right_ptr]
