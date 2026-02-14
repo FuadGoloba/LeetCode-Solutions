49d63286a9b26999a3126282570c9309be7431b1
FuadGoloba <fuadgoloba@gmail.com>
2026-02-14 13:03:37 +0000

    Sliding Window: longest substring without repeating characters

--- Sliding Window/Medium/longest_substring_without_repeating_characters.py
+++ Sliding Window/Medium/longest_substring_without_repeating_characters.py
@@ -25,11 +25,23 @@
 
 def lengthOfLongestSubstring(s):
     '''
-        Using a Sliding Window and Hashset to keep track of duplicate char; Time = O(n), Memory = O(n)
-        
-        SOLUTION:
-            The intuition is to use a sliding window technique and a hashset. We create a window using two pointers (l and r) and adding characters to the hashset and computing the length of the unique substring added. (l for marking the start of a window and r for traversing the string)
-            When we encounter a character that's already in the hashset, we shrink the window, removing the characters from the hashset and adjusting the left pointer till we start a new window (i.e hashset is empty for the new window).
+        Returns the length of the longest substring without repeating characters in a given string s. Using a Sliding Window and Hashset to keep track of duplicate characters.
+
+        Intuition:
+            we can keep one window that always has unique characters. We expand the window by moving the right pointer.
+            If we ever see a repeated character, we shrink the window from the left until the duplicate is removed.
+            This way, the window always represents a valid substring, and we track its maximum size.
+            It's efficient because each character is added and removed at most once.
+            
+        Steps:
+            1. Initialize a hashset and two pointers (left, right).
+            2. Move right pointer, adding unique characters to the set.
+            3. If a duplicate is found, remove characters from the set by moving left pointer.
+            4. Track and update the maximum window size.
+            5. Return the maximum length found.
+            
+        Time Complexity: O(n) - Each character is visited at most twice (once by right pointer and once by left pointer).
+        Space Complexity: O(n) - In the worst case, the hashset can contain all unique characters in the string.
     '''
     seen = set() # Initiliase a hashset to keep track of character's we've come across
     left = 0 # Left pointer (start of a window)
@@ -47,33 +59,54 @@ def lengthOfLongestSubstring(s):
 
 def lengthOfLongestSubstring2(s: str) -> int:
     """
-        Using a Sliding Window and Hashset to keep track of duplicate char; Time = O(n), Memory = O(n)
+        Returns the length of the longest substring without repeating characters in a given string s. Using a Sliding Window and Hashset to keep track of duplicate characters
         
-        SOLUTION:
-            The intuition is to use a sliding window technique and a hashset. We create a window using two pointers (l and r) and adding characters to the hashset and computing the length of the unique substring added. (l for marking the start of a window and r for traversing the string)
-            When we encounter a character that's already in the hashset, we shrink the window, removing the characters from the hashset and adjusting the left pointer till we start a new window (i.e hashset is empty for the new window).
+        Intuition:
+            we can keep one window that always has unique characters. We expand the window by moving the right pointer.
+            If we ever see a repeated character, we shrink the window from the left until the duplicate is removed.
+            This way, the window always represents a valid substring, and we track its maximum size.
+            It's efficient because each character is added and removed at most once.
+            
+        Steps:
+            1. Initialize a hashset and two pointers (st_wdw, end_wdw).
+            2. Move end_wdw pointer, adding unique characters to the set.
+            3. If a duplicate is found, remove characters from the set by moving st_wdw pointer.
+            4. Track and update the maximum window size.
+            5. Return the maximum length found. 
+            
+        Time Complexity: O(n) - Each character is visited at most twice (once by end_wdw pointer and once by st_wdw pointer).
+        Space Complexity: O(n) - In the worst case, the hashset can contain all unique characters in the string.
     """
-    max_length = 0
-    l, r = 0, 0
-    hashset = set()
+    st_wdw, end_wdw, max_len = 0, 0, 0
+    window = set()
 
-    while r < len(s):
-        if s[r] not in hashset:
-            hashset.add(s[r])
-            max_length = max(max_length, (r-l) + 1)
-            r += 1 # Only move to the next character if we haven't repeated characters in a window
+    while end_wdw < len(s):
+        if s[end_wdw] in window:
+            window.remove(s[st_wdw])
+            st_wdw += 1
         else:
-            # Keep removing characters from the hashset and adjusting the left pointer till all elements are removed and we can start a new window 
-            hashset.remove(s[l])
-            l += 1
-    return max_length
+            window.add(s[end_wdw])
+            max_len = max(max_len, (end_wdw - st_wdw) + 1)
+            end_wdw += 1
+    return max_len
 
 def lengthOfLongestSubstring3(s: str) -> int:
     """
-        Using a Sliding Window and a hashmap to keep track of duplicates characters and their count; Time = O(n), Memory = o(n)
+        Returns the length of the longest substring without repeating characters in a given string s. Using a Sliding Window and Hashmap to keep track of duplicate characters and their counts.
         
-        SOLUTION:
-            The intuition follows similar to using a hashset except that you use a hashmap to store the characters and their counts. Deduct the count when a repeated character is encountered.
+        Intuition:
+            The intuition follows similar to using a hashset except that you use a hashmap to store the characters and their counts. 
+            Deduct the count when a repeated character is encountered.
+            
+        Steps:
+            1. Initialize a hashmap and two pointers (left, right).
+            2. Move right pointer, adding unique characters to the hashmap and their counts.
+            3. If a duplicate is found, deduct the count of characters from the hashmap by moving left pointer until the duplicate is removed.
+            4. Track and update the maximum window size.
+            5. Return the maximum length found.
+
+        Time Complexity: O(n) - Each character is visited at most twice (once by right pointer and once by left pointer).
+        Space Complexity: O(n) - In the worst case, the hashmap can contain all unique characters in the string.
     """
     
     map = {}
@@ -91,11 +124,21 @@ def lengthOfLongestSubstring3(s: str) -> int:
 
 def lengthOfLongestSubstring4(s: str) -> int:
     """
-        Using a Sliding Window and a fixed array with size (128) of all english letters, digits, symbols and spaces; to keep track of character count whereby the ascii number of the character represents the index.
-        ; Time = O(n), Memory = o(n) ; Most efficient method
-        
-        SOLUTION:
-            The intuition follows similar to using a hashset/hashmap except that you use an array of fixed size to store the character counts. Deduct the count when a repeated character is encountered.
+        Returns the length of the longest substring without repeating characters in a given string s. Using a Sliding Window and an array to keep track of duplicate characters and their counts.
+
+        Intuition:
+            The intuition follows similar to using a hashset/hashmap except that you use an array of fixed size to store the character counts. 
+            Deduct the count when a repeated character is encountered.
+            
+        Steps:
+            1. Initialize an array of size 128 (assuming ASCII) and two pointers (left, right).
+            2. Move right pointer, adding unique characters to the array and their counts.
+            3. If a duplicate is found, deduct the count of characters from the array by moving left pointer until the duplicate is removed.
+            4. Track and update the maximum window size.
+            5. Return the maximum length found. 
+            
+        Time Complexity: O(n) - Each character is visited at most twice (once by right pointer and once by left pointer).
+        Space Complexity: O(1) - The array size is fixed (128 for ASCII), regardless of the input string size.
     """
     max_len = 0
     left = 0
